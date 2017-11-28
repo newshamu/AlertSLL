a890a4a02eb8b4548808378d0a25b90ab8864515
Chris <csjohns6@ncsu.edu>
2017-11-28 14:25:34 -0500

    Updated sensor information.

--- myModules.js
+++ myModules.js
@@ -34,48 +34,54 @@ exports.ensureDirectoryExistence = function(filePath) {
 // Pressure sensors in JSON form
 exports.sensor1 = {
     "sensorID":"Sensor 1",
-    "yIntercept":"1",
-    "slope":"1"
+    "yIntercept":"300.7",
+    "slope":"1.451"
 }
 
 exports.sensor2 = {
     "sensorID":"Sensor 2",
-    "yIntercept":"2",
-    "slope":"2"
+    "yIntercept":"220.29",
+    "slope":"1.5294"
 }
 
 exports.sensor3 = {
     "sensorID":"Sensor 3",
-    "yIntercept":"3",
-    "slope":"3"
+    "yIntercept":"59.727",
+    "slope":"1.5537"
 }
 
 exports.sensor4 = {
     "sensorID":"Sensor 4",
-    "yIntercept":"4",
-    "slope":"4"
+    "yIntercept":"143.73",
+    "slope":"1.0885"
 }
 
 exports.sensor5 = {
     "sensorID":"Sensor 5",
-    "yIntercept":"5",
-    "slope":"5"
+    "yIntercept":"4.628",
+    "slope":"1.38"
 }
 
 exports.sensor6 = {
     "sensorID":"Sensor 6",
-    "yIntercept":"6",
-    "slope":"6"
+    "yIntercept":"158.12",
+    "slope":"1.1327"
 }
 
 exports.sensor7 = {
     "sensorID":"Sensor 7",
-    "yIntercept":"7",
-    "slope":"7"
+    "yIntercept":"172.27",
+    "slope":"1.0071"
 }
 
 exports.sensor8 = {
     "sensorID":"Sensor 8",
-    "yIntercept":"8",
-    "slope":"8"
+    "yIntercept":"-23.171",
+    "slope":"1.1008"
+}
+
+exports.sensor9 = {
+    "sensorID":"Sensor 9",
+    "yIntercept":"141.23",
+    "slope":"0.9386"
 }
